057a91416c8cadf0fa6478a68b488ac9467ab640
Phumphathai Chansriwong <ouoam2555@gmail.com>
2019-07-21 18:34:38 +0700

    change host

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-// module.exports.connect = "https://aaaaaa.19991999.xyz";
-module.exports.connect = "http://127.0.0.1:1323";
+module.exports.connect = "https://aaaaaa.19991999.xyz";
+// module.exports.connect = "http://127.0.0.1:1323";
 module.exports.path = "/ou";
